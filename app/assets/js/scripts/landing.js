8e1fba8d6f394862faa85dd4e58eaea60b76886c
Nicolas Demeude <nicolasdemeude@icloud.com>
2023-04-02 20:04:40 +0200

    Translations of Player, server etc...

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -243,14 +243,14 @@ const refreshServerStatus = async (fade = false) => {
     loggerLanding.info('Refreshing Server Status')
     const serv = (await DistroAPI.getDistribution()).getServerById(ConfigManager.getSelectedServer())
 
-    let pLabel = 'SERVER'
-    let pVal = 'OFFLINE'
+    let pLabel = 'Serveur'
+    let pVal = 'Eteint'
 
     try {
 
         const servStat = await getServerStatus(47, serv.hostname, serv.port)
         console.log(servStat)
-        pLabel = 'PLAYERS'
+        pLabel = 'Joueurs'
         pVal = servStat.players.online + '/' + servStat.players.max
 
     } catch (err) {
